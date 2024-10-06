5c999e833899755252cde482262a02681fbd487b
festiwise <wasiedu1234@gmail.com>
2024-10-06 11:27:22 +0000

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "put your session Id",
+  SESSION_ID: process.env.SESSION_ID Bmwmd$3MUSWUwq",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : false, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
